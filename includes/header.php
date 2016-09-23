ff819f636ad509470c8889aed0e479038e325b42
Konstantin Semenov <kvsemenoff@mail.ru>
2016-09-23 12:55:12 +0400

    d

--- includes/header.php
+++ includes/header.php
@@ -1,42 +1,4 @@
 
-
-	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
-	<link rel="stylesheet" href="libs/font-awesome-4.2.0/css/font-awesome.min.css" />
-	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
-	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
-	<link rel="stylesheet" href="libs/wow/animate.css">
-	<link type="text/css" href="css/jquery.jscrollpane.css" rel="stylesheet" media="all" />
-
-	<link rel="shortcut icon" href="favicon.ico" />
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="css/style2.css">
-	<link rel="stylesheet" href="css/style3.css">
-
-	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Droid+Sans" />
-	
-	<link rel="stylesheet" href="css/fonts.css">
-
-	<meta content="telephone=no" name="format-detection"/>
-	
-
-</head>
-<body>
-	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
-	<script src="libs/owl.carousel/owl.carousel.js"></script>
-	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
-	<script src="libs/wow/wow.min.js"></script>
-	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
-	<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
-
-	<script>
-    	new WOW().init();
-    </script>
-
-	<script src="js/responsiveTabs.js"></script>
-	<script src="js/jquery.maskedinput.min.js"></script>
-	<script src="js/common.js"></script>
-
-
 	<header class="container">
 		<div class="row">
 			<div class="col-md-12">
--- index.php
+++ index.php
@@ -5,21 +5,56 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Главная страница</title>
 
-	<?php require_once('includes/header.php'); ?>
-	<?php require_once('includes/header2.php'); ?>
-	<?php require_once('includes/block1.php'); ?>
-	<?php require_once('includes/block2.php'); ?>
-	<?php require_once('includes/about.php'); ?>
-	<?php require_once('includes/block3.php'); ?>
-	<?php require_once('includes/request.php'); ?>
-	<?php require_once('includes/footer.php'); ?>
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/font-awesome-4.2.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+	<link rel="stylesheet" href="libs/wow/animate.css">
+	<link type="text/css" href="css/jquery.jscrollpane.css" rel="stylesheet" media="all" />
+
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/style2.css">
+	<link rel="stylesheet" href="css/style3.css">
+
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Droid+Sans" />
+	
+	<link rel="stylesheet" href="css/fonts.css">
+
+	<meta content="telephone=no" name="format-detection"/>
 	
 
+</head>
+<body>
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/owl.carousel/owl.carousel.js"></script>
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="libs/wow/wow.min.js"></script>
+	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+	<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
+
 	<script>
-		$(document).ready(function() {
-			
-		});
-	</script>
+    	new WOW().init();
+    </script>
+
+	<script src="js/responsiveTabs.js"></script>
+	<script src="js/jquery.maskedinput.min.js"></script>
+	<script src="js/common.js"></script>
+
+
+	<?php require_once('includes/header.php'); ?>
+	<?php require_once('includes/ah-slider.php'); ?>
+	<?php require_once('includes/ah-akci.php'); ?>
+	<?php require_once('includes/ah-news.php'); ?>
+
+	<?php require_once('includes/an-block1.php'); ?>
+	<?php require_once('includes/an-block2.php'); ?>
+	<?php require_once('includes/an-block3.php'); ?>
+	<?php require_once('includes/ah-info.php'); ?>
+	<?php require_once('includes/an-block4.php'); ?>
+	<?php require_once('includes/block3.php'); ?>
+	<?php require_once('includes/footer.php'); ?>
+	
 	
 </body>
 </html>
