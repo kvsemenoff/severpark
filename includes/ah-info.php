2835266a0ce821bee52ecf895962f1a3612f5252
hatuhov <hatuhov@yandex.ru>
2016-09-24 02:07:07 +0300

    a

--- includes/ah-info.php
+++ includes/ah-info.php
@@ -1,11 +1,135 @@
 <div class="clearfix"></div>
 
 <section class="ah-info">
-	    <div class="container">
-		    <div class="row">
-				<div class="col-md-12 col-sm-12 col-xs-12">
-					
+    <div class="container">
+	    <div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<h2 class="ah-h2">информация</h2>
+				<div class="row">
+					<div class="ah-info-list">
+						<div class="ah-infowrap1">
+							<div class="ah-infobox">
+								<img src="img/ah-info1.png" class="img-responsive" alt="ah-info1">
+							</div>
+							<a href="#" class="ah-info-link">Дома из кирпича</a>
+							<div class="ah-info-text">
+								111111111111111111111111111111<br>
+								111111111111111111111111111111<br>
+								111111111111111111111111111111<br>
+								111111111111111111111111111111<br>
+							</div>
+						</div>
+						<div class="ah-infowrap1">
+							<div class="ah-infobox">
+								<img src="img/ah-info2.png" class="img-responsive" alt="ah-info2">
+							</div>
+							<a href="#" class="ah-info-link">Чистый воздух</a>
+							<div class="ah-info-text">
+								222222222222222222222222222222<br>
+								222222222222222222222222222222<br>
+								222222222222222222222222222222<br>
+								222222222222222222222222222222<br>
+							</div>
+						</div>
+						<div class="ah-infowrap1">
+							<div class="ah-infobox">
+								<img src="img/ah-info3.png" class="img-responsive" alt="ah-info3">
+							</div>
+							<a href="#" class="ah-info-link">Прогулки и спорт</a>
+							<div class="ah-info-text">
+								3333333333333333333333333333333<br>
+								3333333333333333333333333333333<br>
+								3333333333333333333333333333333<br>
+								3333333333333333333333333333333<br>
+							</div>
+						</div>
+						<div class="ah-infowrap1">
+							<div class="ah-infobox">
+								<img src="img/ah-info4.png" class="img-responsive" alt="ah-info4">
+							</div>
+							<a href="#" class="ah-info-link">Красивая архитектура</a>
+							<div class="ah-info-text">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore laboriosam expedita consectetur odio excepturi autem, quia, consequuntur aut doloribus, molestias nostrum at ratione ullam. Molestias quibusdam rem earum voluptatem voluptate.
+							</div>
+						</div>
+						<div class="ah-infowrap1">
+							<div class="ah-infobox">
+								<img src="img/ah-info5.png" class="img-responsive" alt="ah-info5">
+							</div>
+							<a href="#" class="ah-info-link">Много парковочных<br>мест</a>
+							<div class="ah-info-text">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore laboriosam expedita consectetur odio excepturi autem, quia, consequuntur aut doloribus, molestias nostrum at ratione ullam. Molestias quibusdam rem earum voluptatem voluptate.
+							</div>
+						</div>
+						<div class="ah-infowrap1">
+							<div class="ah-infobox">
+								<img src="img/ah-info6.png" class="img-responsive" alt="ah-info6">
+							</div>
+							<a href="#" class="ah-info-link">Безбарьерная среда</a>
+							<div class="ah-info-text">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore laboriosam expedita consectetur odio excepturi autem, quia, consequuntur aut doloribus, molestias nostrum at ratione ullam. Molestias quibusdam rem earum voluptatem voluptate.
+							</div>
+						</div>
+						<div class="ah-infowrap1">
+							<div class="ah-infobox">
+								<img src="img/ah-info7.png" class="img-responsive" alt="ah-info7">
+							</div>
+							<a href="#" class="ah-info-link">Технологии связи</a>
+							<div class="ah-info-text">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore laboriosam expedita consectetur odio excepturi autem, quia, consequuntur aut doloribus, molestias nostrum at ratione ullam. Molestias quibusdam rem earum voluptatem voluptate.
+							</div>
+						</div>
+						<div class="ah-infowrap1">
+							<div class="ah-infobox">
+								<img src="img/ah-info8.png" class="img-responsive" alt="ah-info8">
+							</div>
+							<a href="#" class="ah-info-link">Экология</a>
+							<div class="ah-info-text">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore laboriosam expedita consectetur odio excepturi autem, quia, consequuntur aut doloribus, molestias nostrum at ratione ullam. Molestias quibusdam rem earum voluptatem voluptate.
+							</div>
+						</div>
+						<div class="ah-infowrap1">
+							<div class="ah-infobox">
+								<img src="img/ah-info9.png" class="img-responsive" alt="ah-info9">
+							</div>
+							<a href="#" class="ah-info-link">Транспортная доступность</a>
+							<div class="ah-info-text">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore laboriosam expedita consectetur odio excepturi autem, quia, consequuntur aut doloribus, molestias nostrum at ratione ullam. Molestias quibusdam rem earum voluptatem voluptate.
+							</div>
+						</div>
+						<div class="ah-infowrap1">
+							<div class="ah-infobox">
+								<img src="img/ah-info10.png" class="img-responsive" alt="ah-info10">
+							</div>
+							<a href="#" class="ah-info-link">Расположение</a>
+							<div class="ah-info-text">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore laboriosam expedita consectetur odio excepturi autem, quia, consequuntur aut doloribus, molestias nostrum at ratione ullam. Molestias quibusdam rem earum voluptatem voluptate.
+							</div>
+						</div>
+						<div class="ah-infowrap1">
+							<div class="ah-infobox">
+								<img src="img/ah-info11.png" class="img-responsive" alt="ah-info11">
+							</div>
+							<a href="#" class="ah-info-link">Что будет построено?</a>
+							<div class="ah-info-text">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore laboriosam expedita consectetur odio excepturi autem, quia, consequuntur aut doloribus, molestias nostrum at ratione ullam. Molestias quibusdam rem earum voluptatem voluptate.
+							</div>
+						</div>
+					</div>
 				</div>
-           </div>
-        </div>
+			</div>
+       </div>
+    </div>
 </section>
+
+<script>
+	$(function(){
+		$('.ah-info-link').mousemove(function(e) {
+        $('.ah-info-text').show();
+		$('.ah-info-text').css('top','10px').css('left',e.clientX + 10);
+	    }).mouseout(function(){
+	    	$('.ah-info-text').hide();
+	    });
+	});
+
+</script>
