c9f578b9caffe47eff4fad0f3551c448aacff9be
hatuhov <hatuhov@yandex.ru>
2016-09-23 13:51:20 +0300

    s

--- includes/ah-slider.php
+++ includes/ah-slider.php
@@ -4,7 +4,26 @@
 	    <div class="container">
 		    <div class="row">
 				<div class="col-md-12 col-sm-12 col-xs-12">
-					
+					<div class="ah-slidbox">
+						<div class="item">
+							<div class="ah-imgbox">
+								<a href="img/ah-slider1.jpg" rel="group"><img src="img/ah-slider1.jpg" alt="ah-slider1"></a>
+							</div>
+						<div class="clear"></div>
+						</div>
+						<div class="item">
+							<div class="ah-imgbox">
+								<a href="img/ah-slider2.jpg" rel="group"><img src="img/ah-slider2.jpg" alt="ah-slider2"></a>
+							</div>
+						<div class="clear"></div>
+						</div>
+						<div class="item">
+							<div class="ah-imgbox">
+								<a href="img/ah-slider3.jpg" rel="group"><img src="img/ah-slider3.jpg" alt="ah-slider3"></a>
+							</div>
+						<div class="clear"></div>
+						</div>
+					</div>
 				</div>
            </div>
         </div>
